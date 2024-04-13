e60cd399d3b6fa9e69377323978baab4f00c25d0
angongar <antoniomanuel2094@gmail.com>
2024-04-13 09:56:53 +0200

    Primer commit: estructura base del proyecto

---
+++ src/main/java/com/tonigdev/api/nikelao/ApiNikelaoApplication.java
@@ -0,0 +1,13 @@
+package com.tonigdev.api.nikelao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNikelaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNikelaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tonigdev/api/nikelao/ApiNikelaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tonigdev.api.nikelao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNikelaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
