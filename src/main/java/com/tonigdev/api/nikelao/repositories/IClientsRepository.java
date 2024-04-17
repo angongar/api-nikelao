e37d3177f5370e4d0d532e172e1a4930b0828623
angongar <antoniomanuel2094@gmail.com>
2024-04-17 15:58:12 +0200

    Se añaden los repositorios del modelo de datos que hemos creado anteriormente

---
+++ src/main/java/com/tonigdev/api/nikelao/repositories/IClientsRepository.java
@@ -0,0 +1,9 @@
+package com.tonigdev.api.nikelao.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.tonigdev.api.nikelao.model.Clients;
+
+public interface IClientsRepository extends CrudRepository<Clients, Long>{
+
+}
---
+++ src/main/java/com/tonigdev/api/nikelao/repositories/IDatesRepository.java
@@ -0,0 +1,9 @@
+package com.tonigdev.api.nikelao.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.tonigdev.api.nikelao.model.Dates;
+
+public interface IDatesRepository extends CrudRepository<Dates, Long>{
+
+}
